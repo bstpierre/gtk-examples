c4341d8505e2068682d11dd930d1bb0933d90538
Brian St. Pierre <brian@bstpierre.org>
2012-05-14 21:00:53 -0400

    Add signal handler to the gtk_builder / glade example

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ SRCS := $(wildcard *.c)
 APPS := $(patsubst %.c,%,$(wildcard *.c))
 
 CFLAGS := -O2 -Wall -Wextra -Werror
-CFLAGS += $(shell pkg-config --cflags --libs gtk+-3.0)
+CFLAGS += $(shell pkg-config --cflags --libs gtk+-3.0 gmodule-2.0)
 
-LIBS := $(shell pkg-config --libs gtk+-3.0)
+LIBS := $(shell pkg-config --libs gtk+-3.0 gmodule-2.0)
 
 all: $(APPS)
 
--- builder.c
+++ builder.c
@@ -1,6 +1,17 @@
 // This example demonstrates the use of a glade UI file.
+//
+// Note: This file must be compiled with the linker flags emitted by
+// $(pkg-config gmodule-2.0) so that the dynamic loader can find the
+// signal handler at runtime. See the Makefile for implementation.
 
 #include <gtk/gtk.h>
+#include <stdio.h>
+
+G_MODULE_EXPORT void handle_button_click(GtkButton* button,
+                                         gpointer user_data)
+{
+  printf("click: %p, %X\n", button, *(int*)user_data);
+}
 
 int main(int argc, char** argv)
 {
@@ -16,7 +27,12 @@ int main(int argc, char** argv)
     GObject* window = gtk_builder_get_object(builder, "main_window");
     gtk_widget_show(GTK_WIDGET(window));
 
-    // TODO: add signal handlers/actions.
+    // Connect the signal handlers defined in the glade file.
+    // (Note: if you're looking for the c++ way to do this, there's no
+    // support for binding C++ signal handlers. You must use 'extern
+    // "C"' functions as handlers.)
+    int my_user_data = 0xDEADBEEF;
+    gtk_builder_connect_signals(builder, &my_user_data);
 
     // Quit the app when the window is closed.
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
